3240c531734dfc3ee4091f50d624580a2057ebd9
Gimer Cervera <gimer.cervera@settlenetwork.com>
2026-02-17 16:10:01 -0600

    fix: user sign-up

--- src/controllers/auth.rs
+++ src/controllers/auth.rs
@@ -1,26 +1,35 @@
-use actix_web::{post, Responder, web};
+use actix_web::{post, Responder, web, HttpResponse};
 use serde::Deserialize;
+use serde_json::json;
 use tokio::sync::MutexGuard;
 use sqlx::{Pool, MySql};
 
 use crate::{db, AppState};
 
 #[derive(Deserialize, Debug)]
-struct SignUpRequest {
-    email: String,
-    password: String,
-    firstname: String,
-    lastname: String,
+pub struct SignUpRequest {
+    pub email: String,
+    pub password: String,
+    pub firstname: String,
+    pub lastname: String,
 }
 
 #[post("/auth/sign-up")]
 pub async fn sign_up(state: web::Data<AppState>, data: web::Json<SignUpRequest>) -> impl Responder {
     let db: MutexGuard<'_, Pool<MySql>> = state.db.lock().await;
     if db::user::has_user_with_email_exists(&db, &data.email).await {
-        return "User already exists".to_string();
+        return HttpResponse::UnprocessableEntity().json(json!({
+            "status": "error",
+            "message": "Email already exists"
+        }));
     }
 
-    format!("Sign up: {:?}", data)
+    db::user::create_user(&db, data.into_inner()).await;
+
+    HttpResponse::Created().json(json!({
+        "status": "success",
+        "message": "User created successfully"
+    }))
 }
 
 #[post("/auth/sign-in")]
--- src/db/user.rs
+++ src/db/user.rs
@@ -1,7 +1,21 @@
+use crate::controllers::auth::SignUpRequest;
+use bcrypt::{DEFAULT_COST, hash};
+
 pub async fn has_user_with_email_exists(db: &sqlx::MySqlPool, email: &str) -> bool {
     sqlx::query!("SELECT * FROM users WHERE email = ?", email)
         .fetch_optional(db)
         .await
         .unwrap()
         .is_some()
+}
+
+pub async fn create_user(db: &sqlx::MySqlPool, user: SignUpRequest) -> bool {
+    let hashed_password = hash(&user.password, DEFAULT_COST).unwrap();
+    sqlx::query!(
+        "INSERT INTO users (email, firstname, lastname, password) VALUES (?, ?, ?, ?)",
+        &user.email, &user.firstname, &user.lastname, &hashed_password
+    )
+    .execute(db)
+    .await
+    .is_ok()
 }
